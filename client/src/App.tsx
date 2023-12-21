a2885951306518f6135f3a414872d569e668076a
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-12-20 22:08:00 -0500

    Create simple websocket client in the app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,23 @@
+import { useEffect, useRef } from 'react'
+
 export default function App() {
+  const connection = useRef<WebSocket | null>(null)
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:3000')
+
+    socket.addEventListener('open', () => {
+      socket.send('Connection established')
+    })
+
+    socket.addEventListener('message', (event) => {
+      console.log('Server message', JSON.parse(event.data))
+    })
+
+    connection.current = socket
+
+    return () => socket.close()
+  }, [])
+
   return null
 }
