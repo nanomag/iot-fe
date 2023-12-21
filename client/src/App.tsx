fa34b08e11d5f4e9ea3cb10f5fc6757f86deef72
Mariano Aguirre Giraldo <nanomag.ag@gmail.com>
2023-12-20 23:14:33 -0500

    Improve socket implementation and support connect/disconnect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef, useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
-type Sensor = {
+interface Sensor {
   id: string
   name: string
   connected: boolean
@@ -15,29 +15,75 @@ export default function App() {
   useEffect(() => {
     const socket = new WebSocket('ws://localhost:3000')
 
-    socket.addEventListener('open', () => {
-      socket.send('Connection established')
-    })
+    socket.onopen = () => {
+      console.log('Connection opened')
+      connection.current = socket
+    }
 
-    socket.addEventListener('message', (event) => {
-      const sensor = JSON.parse(event.data)
-      setSensors((prevSensors) => [...prevSensors, sensor])
-    })
+    socket.onmessage = (event) => {
+      const updatedSensor: Sensor = JSON.parse(event.data.toString())
 
-    connection.current = socket
+      setSensors((prevSensors: Sensor[]) => {
+        const hasSensor = prevSensors.some(
+          (sensor) => sensor.id === updatedSensor.id,
+        )
+
+        if (hasSensor) {
+          return prevSensors.map((sensor) =>
+            sensor.id === updatedSensor.id
+              ? { ...sensor, ...updatedSensor }
+              : sensor,
+          )
+        } else {
+          return [...prevSensors, updatedSensor]
+        }
+      })
+    }
+
+    socket.onerror = (error) => {
+      console.error(`Connection error: ${error}`)
+    }
+
+    socket.onclose = () => {
+      console.log('Connection closed')
+    }
 
     return () => socket.close()
   }, [])
+
+  function connectSensor(id: string) {
+    if (
+      connection.current &&
+      connection.current.readyState === WebSocket.OPEN
+    ) {
+      connection.current.send(JSON.stringify({ command: 'connect', id }))
+    }
+  }
+
+  function disconnectSensor(id: string) {
+    if (
+      connection.current &&
+      connection.current.readyState === WebSocket.OPEN
+    ) {
+      connection.current.send(JSON.stringify({ command: 'disconnect', id }))
+    }
+  }
 
   return (
     <div>
       {sensors.map((sensor) => (
         <div key={sensor.id}>
           <strong>{sensor.name}:</strong> {sensor.value} {sensor.unit}
-          <button onClick={() => {}} disabled={sensor.connected}>
+          <button
+            onClick={() => connectSensor(sensor.id)}
+            disabled={sensor.connected}
+          >
             Connect
           </button>
-          <button onClick={() => {}} disabled={!sensor.connected}>
+          <button
+            onClick={() => disconnectSensor(sensor.id)}
+            disabled={!sensor.connected}
+          >
             Disconnect
           </button>
         </div>
